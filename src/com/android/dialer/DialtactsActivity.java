60e40f8006f74e40c565b8925630f56b78ab9678
Umashankar Godachi <umasha@codeaurora.org>
2013-12-26 12:30:32 +0530

    Fix dialer force close.
    If there is no activity to handle the intent
    ACTION_RECOGNIZE_SPEECH, it leads to dialer force close
    on tapping voice search button.
    Add try catch statement to avoid the dialer force close.
    
    Change-Id: Ie59f06608af913cee3f50a03ebceca9616e6bc8f
    CRs-Fixed: 592572

--- src/com/android/dialer/DialtactsActivity.java
+++ src/com/android/dialer/DialtactsActivity.java
@@ -451,11 +451,11 @@ public class DialtactsActivity extends TransactionSafeActivity implements View.O
                 }
                 break;
             case R.id.voice_search_button:
-                final Intent voiceIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 try {
+                    final Intent voiceIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                     startActivityForResult(voiceIntent, ACTIVITY_REQUEST_CODE_VOICE_SEARCH);
-                } catch (Exception e) {
-                    // TODO: handle exception
+                } catch (ActivityNotFoundException e) {
+                    Log.w(TAG, e.toString());
                 }
                 break;
             default: {
